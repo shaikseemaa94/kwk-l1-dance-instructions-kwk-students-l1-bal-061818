8a8bb4ff79663bef4f40e85a612c6ea359e5952d
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-18 20:22:10 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -18,11 +18,14 @@ def proper_stance
   def bob_the_reins
     puts"Holding your arms in front of you, staight and at chest level"
     puts "Cross your right wrist over your left and hold them together"
-    puts "Lift your arm up and down in a loose bouncing movement, in time with the beat of the song.This movement is repeated eight times"
+    puts "Lift your arm up and down in a loose bouncing movement, in time with the beat of the song."
   end
   
   def lasso 
     puts"Start arm movements by holding left arm so the back of your wrist is near your chin, pointing your left elbow staight left."
     puts "Lift right arm and point right elbow diagonally to the right."
-    puts "Raise right arm forearm so that it points staight up and whip it in a small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+    puts "Raise right arm forearm so that it points staight up and whip it in a small circles to the beat of the song, as if you were a cowboy holding a rope lasso."
   end
+  
+  proper_stance
+  
