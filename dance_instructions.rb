96ebbe0a38587c881643a833174da387756bad0f
shaikseemaa94 <seema.shaik94@gmail.com>
2018-06-18 20:07:46 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,5 +1,10 @@
-def Gangnam_Style
+def Lower_Body
 # Code your methods below
  puts "Plant legs far apart, bend knees slighty and keep posture loose"
  puts "Lift right foot"
+ puts "Return right foot to the ground"
+ puts"Finishing with a small skip-step backward"
+ end 
+ 
+ 
   
